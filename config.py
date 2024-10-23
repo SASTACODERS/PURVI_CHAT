1869ba97b7fd282b897c6e2ad330c1ba62f1dc19
SASTACODERS <princekumar34379@gmail.com>
2024-10-23 13:14:48 +0530

    ᴀʟᴘʜᴀ

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ API_HASH = getenv("API_HASH", "05e5e695feb30e25bef47484cc006da7")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", "7403621976"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Purvi_UPdates")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "PURVI_SUPPORT")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "WORLD_ALPHA")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_ALPHA_BABY_lll")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "@Promotionyoutubr")
 
 # Random Start Images
 IMG = [
